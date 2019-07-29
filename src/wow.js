1a1019974ef8d1c5c3441f9c01ea4e53d43261b1
Ny'alotha <nyalothas@gmail.com>
2019-07-30 00:50:06 +0300

    added initial style

---
+++ src/wow.js
@@ -0,0 +1,15 @@
+var WestOfWesteros = {
+
+	init: function () {
+
+		var initialize = function() {
+			// set up events
+		}
+
+		return {
+			init: initialize()
+		}
+	}
+}
+
+WestOfWesteros.init();
